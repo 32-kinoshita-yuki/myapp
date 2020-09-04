a1071ba0404fa5d148ae8494818221b0f842ab57
”32-kinoshita-yukigit config --global user.email ”git@github.com:32-kinoshita-yuki/sample.git <inuyasya.19950406@gmail.com>
2020-09-04 15:27:36 +0000

    20200904

---
+++ database/migrations/2020_09_04_143117_create_influencers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInfluencersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('influencers', function (Blueprint $table) {//influencersテーブル作成
+            $table->bigIncrements('id');
+            $table->string('name'); // 名前を保存するカラム
+            $table->string('gender');  // 性別を保存するカラム
+            $table->string('address_mail');  // メールアドレスを保存するカラム
+            $table->string('tel_num');// 電話番号を保存するカラム
+            $table->string('address');// 住所を保存するカラム
+            $table->string('age');// 年齢を保存するカラム
+            $table->string('sns_kind');// 使用するsnsを保存するカラム
+            $table->string('sns_url');// snsのurlを保存するカラム
+            $table->string('sns_genre');//snsのジャンルを保存するカラム
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('influencers');
+    }
+}
